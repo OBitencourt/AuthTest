6c0ff9c9e61f9f2c52231abbb9f3d376db13ffff
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2025-07-11 14:02:51 +0000

    feat: adding private route with jwt verification

--- server.js
+++ server.js
@@ -18,9 +18,54 @@ dbConnect()
 
 // Public Route 
 
-app.get('/', (req,res) => {
+app.get('/', async (req,res) => {
 
-    res.status(200).json({message: "Tudo certo"})
+    const users = await UserModel.find()
+
+    res.status(200).json({message: "Tudo certo", users})
+})
+
+
+// Check token
+
+const checkToken = (req, res, next) => {
+
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if (!token) {
+        return res.status(401).json({message: "Acesso negado! Faça login."})
+    }
+
+    try {
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret)
+
+        next()
+
+    } catch {
+
+        return res.status(404).json({message: "Token inválido"})
+    }
+
+}
+
+
+// Private Route
+
+app.get('/user/:id', checkToken , async (req,res) => {
+
+    const { id } = req.params
+
+    const user = await UserModel.findById(id, '-password')
+
+    if(!user) {
+        return res.status(404).json({message: "Usuário não encontrado"}) 
+    } else {
+        return res.status(200).json({message: "Usuário encontrado", user}) 
+    }
+
 })
 
 // Create User
