0cdfe6f456b298e5425081d67f6d965173c28618
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2025-07-11 11:20:22 +0000

    feat: adding mongodb connection with mongoose

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+import dbConnect from './src/utils/dbConnect.js'
 
 const app = express()
-
+dotenv.config()
 app.use(express.json())
+
+// Connecting to DB
+
+dbConnect()
+
+// Public Route 
+
+app.get('/', (req,res) => {
+
+    res.status(200).json({message: "Tudo certo"})
+})
+
 
 const port = process.env.PORT || 8080
 app.listen(port, () => {
